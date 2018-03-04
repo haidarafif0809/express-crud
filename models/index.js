07fd09d23bc7e047482139c7ea03d6414be7a12f
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-04 19:14:54 +0700

    Change config json

--- models/index.js
+++ models/index.js
@@ -8,11 +8,20 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+let database_url = "postgres://wtpiucmzhpiilm:2d67f5314e337eafdc7ecaa3660e5133677538fd277f7f6a8b37b162deb83b8a@ec2-54-243-31-34.compute-1.amazonaws.com:5432/ddhle7bodshsjm";
+
+var sequelize = new Sequelize(database_url, {
+                                    dialect: 'postgres',
+                                    protocol: 'postgres',
+                                    dialectOptions: {
+                                        ssl: true
+                                    }
+                                });
 
 fs
   .readdirSync(__dirname)
